42c88aa8bee4f6ada72f0ed90adf223d227cc6bd
evgen-kulik <e.kulik.atom@gmail.com>
2025-05-24 13:16:00 +0200

    fixing

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,27 +1,39 @@
-import asyncio
 import logging
+from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
+import os
+import asyncio
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8013955462:AAFdTzu_PF11w0zosQbLAOixEj1QaszCj9E"
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I'm a bot built with python-telegram-bot.")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 
-async def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    await app.bot.delete_webhook(drop_pending_updates=True)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Привет! Я DeutschCheckBot.")
+
+
+def main() -> None:
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
-    logging.info("Bot started with polling")
-    await app.run_polling()
+
+    async def run():
+        await app.bot.delete_webhook(drop_pending_updates=True)
+        await app.run_polling()
+
+    try:
+        asyncio.get_event_loop().run_until_complete(run())
+    except RuntimeError as e:
+        if "already running" in str(e):
+            asyncio.create_task(run())
+        else:
+            raise
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
